e43a15d9d4e74f38d0ee8181ea9762c9eedd84b3
Jimmy Saenz <jsaenzr@outlook.com>
2021-02-19 13:38:14 -0600

    Crear proyecto core para definir capa de datos y dominio principalmente
    Agregar dependencia de core -> app

--- mapyt/app/src/main/java/me/mapyt/app/presentation/ui/MainActivity.kt
+++ mapyt/app/src/main/java/me/mapyt/app/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package me.mapyt.app
+package me.mapyt.app.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.mapyt.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
