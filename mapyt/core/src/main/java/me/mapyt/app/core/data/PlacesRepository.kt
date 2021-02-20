35f51f1d8b180d0246a3c703c85c3c1e80751638
Jimmy Saenz <jsaenzr@outlook.com>
2021-02-20 01:47:40 -0600

    Crear repositorio para lugares

---
+++ mapyt/core/src/main/java/me/mapyt/app/core/data/PlacesRepository.kt
@@ -0,0 +1,17 @@
+package me.mapyt.app.core.data
+
+import me.mapyt.app.core.domain.entities.Place
+
+class PlacesRepository(private val remoteSource: PlacesRemoteSource) {
+
+    companion object {
+        const val KEYWORD_SEPARATOR = "|"
+    }
+
+    suspend fun searchNearby(keywords: List<String>, location: String, radius: Int): List<Place> {
+        //TODO: implementar local data source
+        return remoteSource.searchNearby(joinKeywords(keywords), location, radius)
+    }
+
+    private fun joinKeywords(keywords: List<String>) = keywords.joinToString(KEYWORD_SEPARATOR)
+}
