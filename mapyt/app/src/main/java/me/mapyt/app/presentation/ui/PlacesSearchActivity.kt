2707eff8b31511e37c9b48ef25e34765ea3f1d16
Jimmy Saenz <jsaenzr@outlook.com>
2021-02-20 13:32:43 -0600

    Agregar search view base para búsqueda por keywords

--- mapyt/app/src/main/java/me/mapyt/app/presentation/ui/PlacesSearchActivity.kt
+++ mapyt/app/src/main/java/me/mapyt/app/presentation/ui/PlacesSearchActivity.kt
@@ -2,10 +2,16 @@ package me.mapyt.app.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuItemCompat
 import me.mapyt.app.R
 import me.mapyt.app.presentation.base.AppActivityBase
 import me.mapyt.app.presentation.base.setFragment
 import me.mapyt.app.presentation.base.setupToolbar
+import timber.log.Timber
 
 class PlacesSearchActivity : AppCompatActivity(), AppActivityBase {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +20,36 @@ class PlacesSearchActivity : AppCompatActivity(), AppActivityBase {
         setup()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_places_search, menu)
+        setupSearchViewFromMenuItem(menu.findItem(R.id.searchMenuItem))
+        return super.onCreateOptionsMenu(menu)
+    }
+
     private fun setup() {
         setupToolbar(R.id.toolbar, R.string.app_name, R.string.search_places)
         setFragment(PlacesSearchFragment.newInstance(), R.id.searchContainerView)
+    }
+
+    private fun setupSearchViewFromMenuItem(searchMenuItem: MenuItem?) {
+        if(searchMenuItem == null) {
+            Timber.e("null searchMenuItem")
+            return
+        }
+        val searchView = searchMenuItem.actionView as? SearchView ?: return
+        with(searchView) {
+            isActivated = false
+        
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    Timber.i("New query: %s", query)
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    return false
+                }
+            })
+        }
     }
 }
